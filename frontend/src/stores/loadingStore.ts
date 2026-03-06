74fa31a7038e96e42cafc73180c2331abbd7fe0b
GabrielRhoden86 <gabrielrhodden@gmail.com>
2026-03-05 21:34:18 -0300

    Limpar cache do git

--- frontend/src/stores/loadingStore.ts
+++
@@ -1,8 +0,0 @@
-import { defineStore } from 'pinia'
-import { ref } from 'vue'
-
-export const useLoadingStore = defineStore('loading', () => {
-  const isLoading = ref<boolean>(false)
-  const globalLoading = ref<boolean>(false)
-  return { isLoading, globalLoading }
-})
--- frontend/vite-env.d.ts
+++
@@ -1,7 +0,0 @@
-// src/vite-env.d.ts
-/// <reference types="vite/client" />
-
-declare module 'vuetify/styles' {
-  const content: any;
-  export default content;
-}
